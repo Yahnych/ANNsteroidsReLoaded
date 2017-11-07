7a3421cc31624064a92e42b6fe1eb6de85aa16e2
danielberenberg <djberenb@uvm.edu>
2017-11-06 22:29:19 -0500

    image io grayscale yay

---
+++ Controller/cmd.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+python3.6 AsteroidsController.py ./../Asteroids/Asteroids &
+python generate_image.py images
--- Controller/generate_image.py
+++ Controller/generate_image.py
@@ -8,9 +8,35 @@ Created on Mon Nov  6 20:42:19 2017
 
 from PIL import Image
 import pyscreenshot
+import time
+import sys,os
 
-im = pyscreenshot.grab(bbox=(0,0,600,600))
+#im = pyscreenshot.grab(bbox=(0,0,600,600))
 #im_grey = Image.open("test_shot.png").convert('LA')
-im_grey = im.convert('LA')
-im_resized = im_grey.resize((64,64),Image.ANTIALIAS)
-im_resized.save("test-64_greyscale.png", dpi=(64,64))
+#im_grey = im.convert('LA')
+#im_resized = im_grey.resize((64,64),Image.ANTIALIAS)
+#im_resized.save("test-64_greyscale.png", dpi=(64,64))
+
+def capture(filepath):
+    im = pyscreenshot.grab(bbox=(0,0,600,600))
+    im_grayscale = im.convert('LA')
+    im_resized   = im_grayscale.resize((64,64),Image.ANTIALIAS)
+    im_resized.save(filepath+'.png')
+
+    pass
+if __name__ == '__main__':
+   
+    try:
+        script   = sys.argv[0]
+        filestem = sys.argv[1]
+    
+    except IndexError:
+        print ("[!!] Usage python {} <filestem>".format(script))
+        sys.exit()
+    
+    for i in range(10):
+        filepath = filestem + "/" +str(i)
+        capture(filepath)
+        time.sleep(0.1)
+
+
